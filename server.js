7ec1d72180fa939e089043f94590eb1636183824
Missaashnaa1 <missaashnaa1@gmail.com>
2023-10-14 00:43:36 +0530

    updating resume link

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect(
+    301,
+    "https://drive.google.com/file/d/1PXjwx4AYp2vCoG2UQ65sCm5IciUhBlEX/view?usp=sharing"
+  );
 });
 
 app.get("/resume", (req, res) => {
   res.redirect(
     301,
-    "https://drive.google.com/file/d/1Tr9TbRKL2p6SIFER53h6d1V-W3cmeocj/view"
+    "https://drive.google.com/file/d/1PXjwx4AYp2vCoG2UQ65sCm5IciUhBlEX/view?usp=sharing"
   );
+});
+
+app.all("*", (req, res) => {
+  res.redirect("/");
 });
 
 app.listen(port, () => {
